98d5501afb78aafc9dcb87d1c636c46b1fd12801
cxmscb <1073216732@qq.com>
2016-09-09 10:50:34 +0800

    code

---
+++ BezierViewAnimation/app/src/main/java/com/cxmscb/cxm/bezierviewanimation/MainActivity.java
@@ -0,0 +1,40 @@
+package com.cxmscb.cxm.bezierviewanimation;
+
+import android.animation.ObjectAnimator;
+import android.graphics.Path;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imageButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton = (ImageButton) findViewById(R.id.imagebtn);
+    }
+
+    public void onImgClick(View view) {
+        ViewPath path = new ViewPath(); //偏移坐标
+        path.moveTo(0,0);
+        path.lineTo(0,500);
+        path.curveTo(-300,200,-600,800,-800,400);
+        
+        ObjectAnimator anim = ObjectAnimator.ofObject(this,"fabLoc",new ViewPathEvaluator(),path.getPoints().toArray());
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        anim.setDuration(3000);
+        anim.start();
+
+    }
+
+
+    public void setFabLoc(ViewPoint newLoc){
+        imageButton.setTranslationX(newLoc.x);
+        imageButton.setTranslationY(newLoc.y);
+    }
+}
